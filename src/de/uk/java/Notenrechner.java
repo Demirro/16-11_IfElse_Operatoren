0d6d55323760ac01c39485bd58ada0f8fb5f0ea5
Théo Bouveyron <44236116+Demirro@users.noreply.github.com>
2020-11-16 15:06:33 +0100

    Add files via upload

---
+++ src/de/uk/java/Notenrechner.java
@@ -0,0 +1,20 @@
+package de.uk.java;
+
+public class Notenrechner {
+
+	public static void main(String[] args) {
+		int erreichtePunktzahl = 50;
+		int maximalePunktzahl = 172;
+		
+		// Errechne die erreichten Prozente und speichere sie als Variable "erreichteProzente"
+		// ... erreichteProzente = ;
+		
+		// if-Block. In den Klammern steht die Bedingung
+		if () {
+			System.out.println("Nicht bestanden! 6");
+		} else if () {
+			System.out.println("Nicht bestanden! 5");
+		}
+		//.... und so weiter
+	}
+}
